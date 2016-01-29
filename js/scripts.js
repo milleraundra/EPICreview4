71dd51c6d98528ea6639bfb184a18746200ad64c
Aundra Miller <miller.aundra@gmail.com>
2016-01-29 10:01:08 -0800

    adds specs and scripts for processing most chosen toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
-function PizzaOrder(size, sauce, toppings){
+function PizzaOrder(size, sauce, meat, cheese, toppings){
   this.size = size;
   this.sauce = sauce;
+  this.meat = meat;
+  this.cheese = cheese;
   this.toppings = toppings;
+};
+
+PizzaOrder.prototype.price = function(){
+  var pizzaPrice = 5;
+//Price calculation based on size
+  if(this.size === "large") {
+    pizzaPrice += 2;
+  }else if(this.size === "medium") {
+    pizzaPrice += 1;
+  }
+  else {}
+
+//Price calculation based on sauce
+  if(this.sauce !== "red") {
+    pizzaPrice += 0.35;
+  } else {}
+
+//Price calculation based on meat
+  for(var i = 0; i < this.meat.length; i++) {
+    if(this.meat[i] !== "pepperoni") {
+      pizzaPrice += 0.75;
+    }
+    else {}
+  }
+//Price calculation based on cheese selection
+
+  if(this.cheese !== "mozzarella" || this.cheese !== "No cheese") {
+    pizzaPrice += 0.15;
+  }else{}
+
+return pizzaPrice;
+
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,42 @@
 describe("PizzaOrder", function() {
-  it("will capture the properties the pizza's size", function() {
-  var Pizza = new PizzaOrder("large");
+  it("will capture the properties the pizza's size, sauce, meats, cheese, and toppings", function() {
+  var Pizza = new PizzaOrder("large", "red", ["pepperoni", "sausage"], "mozzarella", ["bell peppers", "olives"]);
   expect(Pizza.size).to.equal("large");
+  expect(Pizza.sauce).to.equal("red");
+  expect(Pizza.meat).to.eql(["pepperoni", "sausage"]);
+  expect(Pizza.cheese).to.equal("mozzarella");
+  expect(Pizza.toppings).to.eql(["bell peppers", "olives"]);
   });
+
+  it("calculates price of pizza based on size", function() {
+    var Pizza = new PizzaOrder("large", "red", ["pepperoni"], "mozzarella", []);
+    expect(Pizza.price()).to.equal(7);
+  });
+
+  it("calculates the price of a pizza based on a chosen sauce", function() {
+    var Pizza = new PizzaOrder("small", "white garlic", ["pepperoni"], "mozzarella", []);
+    expect(Pizza.price()).to.equal(5.35);
+  });
+
+  it("calculates the price of a pizza based on meat toppings", function() {
+    var Pizza = new PizzaOrder("small", "red", ["sausage", "bacon"], "mozzarella", []);
+    expect(Pizza.price()).to.equal(6.50);
+  });
+
+  it("calculates the price of a pizza based on cheese selection", function() {
+    var Pizza = new PizzaOrder("small", "red", ["pepperoni"], "cheddar", []);
+    expect(Pizza.price()).to.equal(5.15);
+  })
+
+
+
+
+
+
+
+
+
+
+
+
 });
